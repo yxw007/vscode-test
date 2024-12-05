7db81771c81d01d5aa3054a77fbabb8c8c1536fa
Potter <75838259@qq.com>
2024-12-05 21:10:57 +0800

    webview simple

---
+++ src/extension.js
@@ -0,0 +1,134 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+const vscode = __importStar(require("vscode"));
+function activate(context) {
+    const provider = new ColorsViewProvider(context.extensionUri);
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
+    context.subscriptions.push(vscode.commands.registerCommand('calicoColors.addColor', () => {
+        provider.addColor();
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('calicoColors.clearColors', () => {
+        provider.clearColors();
+    }));
+}
+class ColorsViewProvider {
+    _extensionUri;
+    static viewType = 'calicoColors.colorsView';
+    _view;
+    constructor(_extensionUri) {
+        this._extensionUri = _extensionUri;
+    }
+    resolveWebviewView(webviewView, _context, _token) {
+        this._view = webviewView;
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
+            localResourceRoots: [
+                this._extensionUri
+            ]
+        };
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+        webviewView.webview.onDidReceiveMessage(data => {
+            switch (data.type) {
+                case 'colorSelected':
+                    {
+                        vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+                        break;
+                    }
+            }
+        });
+    }
+    addColor() {
+        if (this._view) {
+            this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+            this._view.webview.postMessage({ type: 'addColor' });
+        }
+    }
+    clearColors() {
+        if (this._view) {
+            this._view.webview.postMessage({ type: 'clearColors' });
+        }
+    }
+    _getHtmlForWebview(webview) {
+        // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+        // Do the same for the stylesheet.
+        const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+        const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+        const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+        // Use a nonce to only allow a specific script to be run.
+        const nonce = getNonce();
+        return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<ul class="color-list">
+				</ul>
+
+				<button class="add-color-button">Add Color</button>
+
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+    }
+}
+function getNonce() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+//# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,122 +1,116 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const logProvider = new LogProvider(context.extensionUri);
+
+	const provider = new ColorsViewProvider(context.extensionUri);
 
 	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider("Translate.Logs", logProvider));
-
-	// 注册命令以添加随机日志
-	context.subscriptions.push(
-		vscode.commands.registerCommand('myExtension.addRandomLog', () => {
-			const log = `Random log entry at ${new Date().toLocaleTimeString()}`;
-			logProvider.addLog(log);
-		})
-	);
-
-	// 注册排序日志命令
-	context.subscriptions.push(
-		vscode.commands.registerCommand('myExtension.sortLogsAsc', () => {
-			logProvider.sortLogs('asc');
-		})
-	);
+		vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('myExtension.sortLogsDesc', () => {
-			logProvider.sortLogs('desc');
-		})
-	);
+		vscode.commands.registerCommand('calicoColors.addColor', () => {
+			provider.addColor();
+		}));
 
-	// 注册清除日志命令
 	context.subscriptions.push(
-		vscode.commands.registerCommand('myExtension.clearLogs', () => {
-			logProvider.clearLogs();
-		})
-	);
-
+		vscode.commands.registerCommand('calicoColors.clearColors', () => {
+			provider.clearColors();
+		}));
 }
 
-export function deactivate() { }
+class ColorsViewProvider implements vscode.WebviewViewProvider {
 
-class LogProvider implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'calicoColors.colorsView';
+
 	private _view?: vscode.WebviewView;
 
 	constructor(
 		private readonly _extensionUri: vscode.Uri,
 	) { }
 
-	resolveWebviewView(webviewView: vscode.WebviewView) {
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		_context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
 		this._view = webviewView;
 
 		webviewView.webview.options = {
+			// Allow scripts in the webview
 			enableScripts: true,
+
 			localResourceRoots: [
 				this._extensionUri
 			]
 		};
 
-		webviewView.webview.html = this.getWebviewContent(webviewView.webview);
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-		webviewView.webview.onDidReceiveMessage(
-			message => {
-				switch (message.command) {
-					case 'sort':
-						this.sortLogs(message.order);
+		webviewView.webview.onDidReceiveMessage(data => {
+			switch (data.type) {
+				case 'colorSelected':
+					{
+						vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
 						break;
-					case 'clear':
-						this.clearLogs();
-						break;
-				}
+					}
 			}
-		);
+		});
 	}
 
-	addLog(message: string) {
+	public addColor() {
 		if (this._view) {
-			this._view.webview.postMessage({ command: 'addLog', message });
+			this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+			this._view.webview.postMessage({ type: 'addColor' });
 		}
 	}
 
-	sortLogs(order: 'asc' | 'desc') {
+	public clearColors() {
 		if (this._view) {
-			this._view.webview.postMessage({ command: 'sort', order });
+			this._view.webview.postMessage({ type: 'clearColors' });
 		}
 	}
 
-	clearLogs() {
-		if (this._view) {
-			this._view.webview.postMessage({ command: 'clear' });
-		}
-	}
-
-	private getWebviewContent(webview: vscode.Webview) {
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
 		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+
+		// Do the same for the stylesheet.
 		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+
+		// Use a nonce to only allow a specific script to be run.
 		const nonce = getNonce();
 
-		return `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Translate Logs</title>
-								<link href="${styleResetUri}" rel="stylesheet">
-            </head>
-            <body>
-                <div style="display: flex; justify-content: space-between; align-items: center;">
-                    <h1>Log Panel</h1>
-                    <div>
-                        <button onclick="sortLogs('asc')">Sort Asc</button>
-                        <button onclick="sortLogs('desc')">Sort Desc</button>
-                        <button onclick="clearLogs()">Clear</button>
-                    </div>
-                </div>
-                <div id="logContainer">logContainer</div>
-                <script nonce="${nonce}" src="${scriptUri}"></script>
-            </body>
-            </html>
-        `;
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<ul class="color-list">
+				</ul>
+
+				<button class="add-color-button">Add Color</button>
+
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
 	}
 }
 
